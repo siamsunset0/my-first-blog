3919f01c9f73d97bb6345a1f2f0895f3a97bb4c6
siamsunset0 <siamsunset0@gmail.com>
2016-01-06 19:20:00 +0900

    First DjangoGirls App Commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sunghoonoh/Documents/PythonCoding/Django/djgirls/girlsenv/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
