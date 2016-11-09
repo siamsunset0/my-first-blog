2c533adfc2686b7816ba5c96676d0cc186fa64ff
siamsunset0 <siamsunset0@gmail.com>
2016-11-09 17:50:07 +0900

    re

--- Projects/01PJ/blog/views.py
+++ Projects/01PJ/blog/views.py
@@ -28,7 +28,7 @@ def post_new(request):
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
+    if request.method == "POSTGO":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
@@ -39,4 +39,3 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-
