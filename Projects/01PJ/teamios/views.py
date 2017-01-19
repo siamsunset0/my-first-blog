0404e8e050f7953a8f1fd7edeab28510bd5c3204
siamsunset0 <siamsunset0@gmail.com>
2017-01-19 16:47:42 +0900

    POST

--- Projects/01PJ/teamios/views.py
+++ Projects/01PJ/teamios/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.http import HttpResponse
 from .models import SimpleInfo
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
 def si_list(request):
     simpleinfos = SimpleInfo.objects.order_by('id')
     rtStrValue = simpleinfos[0].siLabel
